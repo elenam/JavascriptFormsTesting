2aa904c61cee2fb19a207eafef21bfacbe0a24b3
elenam <elenam@morris.umn.edu>
2014-01-27 23:23:11 -0600

    Initial commit

---
+++ functions.js
@@ -0,0 +1,26 @@
+/**
+ * Created by elenam on 1/27/14.
+ */
+
+function createParagraph(content) {
+    return "<p>" + content + "</p>"
+}
+
+function createImage(url) {
+    return "<img src ='" + url +"' />";
+}
+
+function createList(n) {
+    var defaultElements = ["One", "Two", "Three", "Four", "Five"];
+    var theList = "<ol>";
+    for (var i = 0; i < n; ++i) {
+        if (i < defaultElements.length) {
+            theList += "<li>" + defaultElements[i] + "</li>";
+        } else {
+            theList += "<li>Infinity</li>";
+        }
+    }
+    theList += "</ol>";
+    return theList;
+}
+
